d5c9e1a8b164182c56143f87e706177ac2de3b5b
Yiter <992433931@qq.com>
2016-10-20 15:54:27 +0800

    reset

--- mytest/demos/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from demos.models import *
-# Register your models here.
-
-admin.site.register(TestTransteaction)
--- mytest/demos/urls.py
+++
@@ -1,6 +0,0 @@
-from  django.conf.urls import url,include
-
-urlpatterns = [
-    url(r'^notransation/$','demos.views.testtransaction'),
-    url(r'^withtransation/$', 'demos.views.testtransaction'),
-]
